1039b6ecbcb2050ac644d598a3dbd64ab09a7a86
saurabh sharma <saurabh.19@outlook.com>
2020-07-05 20:48:42 +0530

    wip 24

--- 24.js
+++ 24.js
@@ -1,11 +1,57 @@
+function factorial(n) {
+  if (n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+function immutableSwap(items, firstIndex, secondIndex) {
+  const results = items.slice();
+  const firstItem = items[firstIndex];
+  results[firstIndex] = items[secondIndex];
+  results[secondIndex] = firstItem;
+
+  return results;
+}
+
+function numberToSwap(numArr) {
+  // iterate in reverse order
+  // because it will increment the list Lexicographic
+  for (let i = numArr.length - 1; i >= 0; i++) {
+    let num = numArr[i];
+    // iterate over the list and find the number
+    // whose right side is bigger than it
+    // because it can be replaced to get a bigger number
+
+    let indexToSwap =
+      numArr.slice(0, i).length -
+      1 -
+      numArr
+        .slice(0, i)
+        .reverse()
+        .findIndex((nextNum) => num > nextNum);
+    if (indexToSwap !== -1) {
+      return [i, indexToSwap];
+    }
+  }
+  return null;
+}
+
 // lets make it work then optimize
-function permuteArr(arr) {
-  let permutations = [];
-  for (let i = 0; i < arr.length; i++) {
-    let arrElement = arr[i];
-    let cloneArr = JSON.parse(JSON.stringify(arr));
-    for (let j = 0; j < arr.length; j++) {}
+function permute(numArr) {
+  const permutations = factorial(numArr.length);
+  // we already have first permutation
+  console.log(numArr);
+  for (let i = 1, lastNumArr = numArr; i <= permutations; i++) {
+    let indexes = numberToSwap(lastNumArr);
+
+    if (indexes === null) {
+      return i;
+    }
+    let [first, second] = indexes;
+    lastNumArr = immutableSwap(lastNumArr, first, second);
+    console.log(lastNumArr);
   }
 }
+
+permute([1, 2, 3, 4]);
 
 function main() {}
