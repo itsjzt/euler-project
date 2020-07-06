17d57d152df7b425fb81606ba554b8cacd917da8
saurabh sharma <saurabh.19@outlook.com>
2020-07-07 04:09:35 +0530

    added 24

--- 24.js
+++ 24.js
@@ -1,57 +1,30 @@
-function factorial(n) {
-  if (n === 1) return 1;
-  return n * factorial(n - 1);
-}
+const permutator = (inputArr) => {
+  let result = [];
+  let i = 1;
 
-function immutableSwap(items, firstIndex, secondIndex) {
-  const results = items.slice();
-  const firstItem = items[firstIndex];
-  results[firstIndex] = items[secondIndex];
-  results[secondIndex] = firstItem;
-
-  return results;
-}
-
-function numberToSwap(numArr) {
-  // iterate in reverse order
-  // because it will increment the list Lexicographic
-  for (let i = numArr.length - 1; i >= 0; i++) {
-    let num = numArr[i];
-    // iterate over the list and find the number
-    // whose right side is bigger than it
-    // because it can be replaced to get a bigger number
-
-    let indexToSwap =
-      numArr.slice(0, i).length -
-      1 -
-      numArr
-        .slice(0, i)
-        .reverse()
-        .findIndex((nextNum) => num > nextNum);
-    if (indexToSwap !== -1) {
-      return [i, indexToSwap];
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      if (i === 1000000) {
+        result = m;
+      }
+      i++;
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next));
+      }
     }
-  }
-  return null;
+  };
+
+  permute(inputArr);
+
+  return result;
+};
+
+function main() {
+  const result = permutator([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).join("");
+  return Number(result);
 }
 
-// lets make it work then optimize
-function permute(numArr) {
-  const permutations = factorial(numArr.length);
-  // we already have first permutation
-  console.log(numArr);
-  for (let i = 1, lastNumArr = numArr; i <= permutations; i++) {
-    let indexes = numberToSwap(lastNumArr);
-
-    if (indexes === null) {
-      return i;
-    }
-    let [first, second] = indexes;
-    lastNumArr = immutableSwap(lastNumArr, first, second);
-    console.log(lastNumArr);
-  }
-}
-
-permute([1, 2, 3, 4]);
-
-function main() {}
+module.exports = main;
---
+++ 24.test.js
@@ -0,0 +1,5 @@
+const main = require("./24");
+
+test("Problem 24", () => {
+  expect(main()).toBe(2783915460);
+});
